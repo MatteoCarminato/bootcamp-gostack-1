62a7cdeffc55eccca7f8fd4193ffc6c8813b2a4a
Matteo Carminato <mcarminato95@gmail.com>
2019-09-07 19:18:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+let NumRequest = 0;
+const projects = [];
+
+/**
+ * Middleware se o projeto existe
+ */
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+/**
+ * Middleware log no número de requisições
+ */
+function logRequests(req, res, next) {
+  NumRequest++;
+
+  console.log(`Número de requisições: ${NumRequest}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+/**
+ * Projects
+ */
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+/**
+ * Tasks
+ */
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3334);
